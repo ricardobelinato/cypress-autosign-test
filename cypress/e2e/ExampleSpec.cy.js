a9f9fd175bab1dd53481bf9d903adbd779a54684
Rubeus Tecnologia e Inovação <ricbelinato@gmail.com>
2024-09-04 09:41:15 -0300

    Adding ExampleSpec.cy.js with exception in .gitignore

---
+++ cypress/e2e/ExampleSpec.cy.js
@@ -0,0 +1,11 @@
+function CONFIG(){
+    return {
+        url: "",
+        ps: 0
+    };
+}
+
+module.exports = {CONFIG}
+
+// require('../support/formSteps/01_EscolhaDePS.cy.js');
+// require('../support/formSteps/02_VarreduraCampos.cy.js');
