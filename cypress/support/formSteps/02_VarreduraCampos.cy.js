8634d723c1207714c70d2428b3dd9a9a5ed8e83a
Rubeus Tecnologia e Inovação <ricbelinato@gmail.com>
2024-10-17 16:30:47 -0300

    creating a function in the config to personalize the candidate

--- cypress/support/formSteps/02_VarreduraCampos.cy.js
+++ cypress/support/formSteps/02_VarreduraCampos.cy.js
@@ -1,5 +1,7 @@
 import { gerarNomeESobrenome, gerarNomeCompleto, gerarEmail, gerarCelular, gerarCPF, gerarDataNascimentoMaior, gerarDataNascimentoMenor, gerarCep, gerarRG, gerarCNPJ } from '../dataGenerator';
 
+const { CANDIDATO } = require('../../config/configSpec');
+
 const { nomeAleatorio, sobrenomeAleatorio } = gerarNomeESobrenome();
 const nomeCompletoAleatorio = gerarNomeCompleto(nomeAleatorio, sobrenomeAleatorio);
 const emailAleatorio = gerarEmail(nomeAleatorio, sobrenomeAleatorio);
@@ -10,55 +12,29 @@ const dataNascimentoMenor = gerarDataNascimentoMenor();
 const cepAleatorio = gerarCep();
 const RGAleatorio = gerarRG();
 const CNPJAleatorio = gerarCNPJ();
-let numCamposPreenchidos = 0;
 
-//Varredura de campos atráves da classe identificadora da div mãe
 describe('Varredura de campos', () => {
-    it('Primeiro passo da inscrição', () => {
+    it('Leitura e preenchimento dos campos do primeiro passo da inscrição', () => {
+        cy.document().then((doc) => {
+            const labels = [
+                { text: 'Nome completo *', value: nomeCompletoAleatorio },
+                { text: 'E-mail *', value: emailAleatorio },
+                { text: 'Celular *', value: celularAleatorio },
+                { text: 'CPF *', value: CPFAleatorio },
+                { text: 'Data de nascimento *', value: dataNascimentoMaior }
+            ];
 
-        cy.get('div').each(($div) => {
-            if ($div.hasClass('nomeCompleto')) {
-                cy.wrap($div).find('input').type(nomeCompletoAleatorio);
-                numCamposPreenchidos += 1;
-            }
-            if ($div.hasClass('email')) {
-                cy.wrap($div).find('input').type(emailAleatorio);
-                numCamposPreenchidos += 1;
-            }
-            if ($div.hasClass('celular')) {
-                cy.wrap($div).find('input').type(celularAleatorio);
-                numCamposPreenchidos += 1;
-            }
-            if ($div.hasClass('cpfRespInsc') || $div.hasClass('cpfCandidato')) {
-                cy.wrap($div).find('input').type(CPFAleatorio);
-                numCamposPreenchidos += 1;
-            }
-            if ($div.hasClass('dataNascimento')) {
-                cy.wrap($div).find('input[type="text"]').type(dataNascimentoMaior);
-                // numCamposPreenchidos += 1;
-            }
-        }).then(() => { //Varredura de campos backup caso não existam classes identificadoras na div mãe, esta pega pelo label do input
-            if (numCamposPreenchidos == 0) {
-                cy.document().then((doc) => {
-                    const labels = [
-                        { text: 'Nome completo *', value: nomeCompletoAleatorio },
-                        { text: 'E-mail *', value: emailAleatorio },
-                        { text: 'Celular *', value: celularAleatorio },
-                        { text: 'CPF *', value: CPFAleatorio },
-                        { text: 'Data de Nascimento *', value: dataNascimentoMaior }
-                    ];
-
-                    labels.forEach(labelObj => {
-                        const label = Array.from(doc.querySelectorAll('label')).find(el => el.textContent.includes(labelObj.text));
-                        if (label) {
-                            cy.wrap(label).parent().find('input').type(labelObj.value);
-                        } else {
-                            cy.log(`O label "${labelObj.text}" não existe no DOM`);
-                        }
+            labels.forEach(labelObj => {
+                const label = Array.from(doc.querySelectorAll('label')).find(el => el.textContent.includes(labelObj.text));
+                if (label) {
+                    cy.wrap(label).parent().find('input').filter(':visible').first().then(input => {
+                        cy.wrap(input).type(labelObj.value);
                     });
-                });
-            }
-        })
+                } else {
+                    cy.log(`O label "${labelObj.text}" não existe no DOM`);
+                }
+            });
+        });
 
         // .then(() => {
         //     cy.get('input[type="radio"]').each(($inputRadio) => {
@@ -77,7 +53,9 @@ describe('Varredura de campos', () => {
         // cy.get('strong').contains('Política de Privacidade').parents('div').prev().find('input[type="checkbox"]').check({ force: true });
         cy.get('input[type="checkbox"]').eq(1).check({ force: true })
 
-        //Exibe campos ocultos
+    });
+
+    it('Exibição de campos ocultos do primeiro passo da inscrição', () => {
         cy.window().then((win) => {
             let i = 0;
             while (i < 10) {
@@ -91,26 +69,9 @@ describe('Varredura de campos', () => {
                 i++;
             }
         });
+    })
 
-
-        // const scriptSrc = 'cypress/support/tracking.service.ts';
-
-        // Cypress.Commands.add('injectScript', (scriptSrc) => {
-        //     cy.window().then((win) => {
-        //         const script = win.document.createElement('script');
-        //         script.type = 'text/javascript';
-        //         script.src = scriptSrc;
-        //         win.document.head.appendChild(script);
-        //     });
-        // });
-
-
-        // cy.intercept('POST', '/api/tracking').as('getTrackingInfo');
+    it('Conclusão do primeiro passo da inscrição', () => {
         // cy.get('button').contains('Avançar').click();
-        // cy.wait('@getTrackingInfo');
-
-    });
-    // it('Segundo passo da inscrição', () => {
-
-    // });
+    })
 });
