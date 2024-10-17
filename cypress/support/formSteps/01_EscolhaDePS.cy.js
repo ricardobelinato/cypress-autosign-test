1f3af658135091f3fc84625f1b23ea7f25b5b9eb
Rubeus Tecnologia e Inovação <ricbelinato@gmail.com>
2024-10-17 15:22:36 -0300

    Improve code in 01_EscolhaDePS by separating actions into individual 'it' blocks, cleaning up, and simplifying the code

--- cypress/support/formSteps/01_EscolhaDePS.cy.js
+++ cypress/support/formSteps/01_EscolhaDePS.cy.js
@@ -1,26 +1,21 @@
 const { CONFIG } = require('../../config/configSpec');
 
 describe('Escolha de PS', module.exports = () => {
-  it('passes', () => {
-    cy.visit(CONFIG().url)
+  it('Navega para a URL', () => {
+    cy.visit(CONFIG().url);
+  })
 
-    cy.window().then((win) => {
-      const script = win.document.createElement('script');
-      script.src = 'https://tracking.apprubeus.com.br/libs/RBTracking.min.js?rbclicod=aRqQCldPocOJ4hUr5nno';
-      script.async = true;
-      win.document.head.appendChild(script);
-    });
-
+  it('Injeção do Tracking', () => {
+    //Método 1
     // cy.window().then((win) => {
     //   const script = win.document.createElement('script');
-    //   script.src = 'https://tracking.apprubeus.com.br'; // URL do script externo
-    //   script.async = true; // Permite carregamento assíncrono
-    //   win.document.head.appendChild(script); // Anexa o script ao head
+    //   script.src = 'https://tracking.apprubeus.com.br/libs/RBTracking.min.js?rbclicod=aRqQCldPocOJ4hUr5nno';
+    //   script.async = true;
+    //   win.document.head.appendChild(script);
     // });
 
-
+    //Método 2
     // cy.get('head').then(($head) => {
-
     //   const script = document.createElement('script');
 
     //   script.type = 'text/javascript';
@@ -28,36 +23,21 @@ describe('Escolha de PS', module.exports = () => {
     //   script.async = true;
 
     //   $head[0].appendChild(script);
-    //   // document.head.appendChild(script);
+    //   document.head.appendChild(script);
 
-    //   // console.log($head[0]);
-    //   // console.log(document.head);
-
+    //   console.log($head[0]);
+    //   console.log(document.head);
     // });
+  })
 
+  it('Interação com mat-selector, escolha do processo seletivo', () => {
+    cy.get('mat-select#mat-select-0').click();
+    cy.get('mat-option').eq(CONFIG().ps).click();
 
-
-
-
-
-    // .then(()=> {
-    //   //Limpar localStorage, sessionStorage, e cookies juntos:
-    //   cy.clearLocalStorage();
-    //   cy.clearCookies();
-    //   cy.window().then((win) => {
-    //     win.sessionStorage.clear();
-    //   });
-
-    // })
-
-
-    cy.get('mat-select#mat-select-0').click()
-    cy.get('mat-option').eq(CONFIG().ps).click()
-
-    cy.get('button.mat-raised-button.mat-button-base.mat-primary').should('be.visible').click()
+    cy.get('button.mat-raised-button.mat-button-base.mat-primary').should('be.visible').click();
 
     cy.get('app-client-loader div').should('exist');
     cy.get('app-client-loader div').should('not.exist');
-    cy.wait(500);
+    cy.wait(700);
   })
 })
